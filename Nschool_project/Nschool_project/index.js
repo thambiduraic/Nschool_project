1a982b98f1269d59dd97beba488fb4a41c94284d
Thambidurai Chennaiyen <74646258+thambiduraic@users.noreply.github.com>
2024-04-02 13:48:21 +0530

    Add files via upload

---
+++ Nschool_project/Nschool_project/index.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("feedback-form").style.display="block";
+}
+function closeNav() {
+    document.getElementById("feedback-form").style.display="none";
+}
+
